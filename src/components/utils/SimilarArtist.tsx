7e02d8062da5e9d90bb7ce38d4cf8fa887bde4d0
padunk <abraham.agel@gmail.com>
2021-08-01 19:35:08 +0700

    chore: add template

--- src/components/utils/SimilarArtist.tsx
+++ src/components/utils/SimilarArtist.tsx
@@ -1,42 +1,42 @@
 import { Avatar, Box, Tag, TagLabel } from "@chakra-ui/react";
 import React from "react";
-import { isTemplateImage } from "../../helpers/utils";
+import { customAvatarTemplate, isTemplateImage } from "../../helpers/utils";
 import { ArtistSimilar } from "../../types";
 
 interface Props {
     artist: ArtistSimilar[];
 }
 
-const customTemplateURL = "/assets/images/undraw_mello_otq1.svg";
-
 const SimilarArtist = ({ artist }: Props) => {
+    console.log(artist);
     return (
         <>
-            {artist.map((artis) => {
-                return (
-                    <Box as="a" href={artis.url} key={artis.name}>
-                        <Tag
-                            size="lg"
-                            colorScheme="blue"
-                            borderRadius="full"
-                            m="1"
-                        >
-                            <Avatar
-                                src={
-                                    isTemplateImage(artis.image[2]["#text"])
-                                        ? customTemplateURL
-                                        : artis.image[2]["#text"]
-                                }
-                                size="xs"
-                                name={artis.name}
-                                ml={-1}
-                                mr={2}
-                            />
-                            <TagLabel>{artis.name}</TagLabel>
-                        </Tag>
-                    </Box>
-                );
-            })}
+            {artist &&
+                artist.map((artis) => {
+                    return (
+                        <Box as="a" href={artis.url} key={artis.name}>
+                            <Tag
+                                size="lg"
+                                colorScheme="blue"
+                                borderRadius="full"
+                                m="1"
+                            >
+                                <Avatar
+                                    src={
+                                        isTemplateImage(artis.image[2]["#text"])
+                                            ? customAvatarTemplate
+                                            : artis.image[2]["#text"]
+                                    }
+                                    size="xs"
+                                    name={artis.name}
+                                    ml={-1}
+                                    mr={2}
+                                />
+                                <TagLabel>{artis.name}</TagLabel>
+                            </Tag>
+                        </Box>
+                    );
+                })}
         </>
     );
 };
