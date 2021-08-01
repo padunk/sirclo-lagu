302179701f66841cb9b8f09dc89a3a4ead474a42
padunk <abraham.agel@gmail.com>
2021-08-01 19:35:38 +0700

    chore: add template url, fixed func params type

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,7 +1,10 @@
-export function isTemplateImage(url: string): boolean {
+export function isTemplateImage(url: string | undefined): boolean {
     const templateURL = "2a96cbd8b46e442fc41c2b86b821562f.png";
-    if (url.includes(templateURL)) {
+    if (typeof url === "string" && url.includes(templateURL)) {
         return true;
     }
     return false;
 }
+
+export const customAvatarTemplate = "/assets/images/undraw_mello_otq1.svg";
+export const customCoverTemplate = "/assets/images/cover_template.jpg";
