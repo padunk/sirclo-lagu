f78f3e1e826a641fff5ea4cd7ba582d62571d49d
padunk <abraham.agel@gmail.com>
2021-08-02 16:09:57 +0700

    feat: add paginated

--- src/components/core/ArtistList.tsx
+++ src/components/core/ArtistList.tsx
@@ -1,32 +1,47 @@
 import { Flex, SimpleGrid } from "@chakra-ui/react";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
-import { TopArtist } from "../../types";
+import { Attributes, TopArtist } from "../../types";
 import ArtistCard from "../utils/ArtistCard";
 import ListLayout from "../utils/ListLayout";
 import ListLoader from "../utils/ListLoader";
+import PaginationBar from "../utils/PaginationBar";
 
 interface Props {}
 
 const ArtistList = (props: Props) => {
-    const getTopArtists = async () => {
-        let result: { data: { artists: TopArtist } };
+    const [pagination, setPagination] = useState<Attributes>({
+        page: "1", // page number can not exceed 1_000_000
+        perPage: "",
+        totalPages: "",
+        total: "",
+    });
+
+    const getTopArtists = async (page = 1, limit = 24) => {
+        let result: {
+            data: {
+                artists: TopArtist;
+            };
+        };
+
         try {
             result = await axios.get(
                 `http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${
                     import.meta.env.VITE_LAST_FM_API_KEY
-                }&format=json&limit=12`
+                }&format=json&limit=${limit}&page=${page}`
             );
         } catch (error) {
-            throw new Error("problem getting top tracks");
+            throw new Error("problem getting top artists");
         }
+        setPagination(result.data.artists["@attr"]);
         return result.data.artists;
     };
 
     const { isLoading, isError, data, error } = useQuery<TopArtist, Error>(
-        "topArtist",
-        getTopArtists
+        ["topArtist", Number(pagination.page)],
+        () => getTopArtists(Number(pagination.page)),
+        { keepPreviousData: true }
     );
 
     if (isError) {
@@ -34,10 +49,14 @@ const ArtistList = (props: Props) => {
     }
 
     return (
-        <ListLayout isLoaded={isLoading}>
+        <ListLayout>
             {data?.artist.map((art) => {
                 return <ArtistCard key={art.name} artist={art.name} />;
             })}
+            <PaginationBar
+                pagination={pagination}
+                setPagination={setPagination}
+            />
         </ListLayout>
     );
 };
--- src/components/core/SongList.tsx
+++ src/components/core/SongList.tsx
@@ -1,32 +1,50 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { Button, Flex, SimpleGrid } from "@chakra-ui/react";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
-import { TopTrack } from "../../types";
+import { Attributes, TopTrack } from "../../types";
 import ListLayout from "../utils/ListLayout";
 import ListLoader from "../utils/ListLoader";
+import PaginationBar from "../utils/PaginationBar";
 import SongCard from "../utils/SongCard";
 
 interface Props {}
 
 const SongList = (props: Props) => {
-    const getTopTracks = async () => {
-        let result: { data: { tracks: TopTrack } };
+    const [pagination, setPagination] = useState<Attributes>({
+        page: "1", // page number can not exceed 1_000_000
+        perPage: "",
+        totalPages: "",
+        total: "",
+    });
+
+    const getTopTracks = async (page = 1, limit = 24) => {
+        let result: {
+            data: {
+                tracks: TopTrack;
+            };
+        };
+
         try {
             result = await axios.get(
                 `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${
                     import.meta.env.VITE_LAST_FM_API_KEY
-                }&format=json&limit=12`
+                }&format=json&limit=${limit}&page=${page}`
             );
         } catch (error) {
             throw new Error("problem getting top tracks");
         }
+        setPagination(result.data.tracks["@attr"]);
         return result.data.tracks;
     };
 
-    const { isLoading, isError, data, error } = useQuery<TopTrack, Error>(
-        "topTracks",
-        getTopTracks
+    const { isLoading, isError, data, error, isPreviousData } = useQuery<
+        TopTrack,
+        Error
+    >(
+        ["topTracks", Number(pagination.page)],
+        () => getTopTracks(Number(pagination.page)),
+        { keepPreviousData: true }
     );
 
     if (isError) {
@@ -44,6 +62,10 @@ const SongList = (props: Props) => {
                     />
                 );
             })}
+            <PaginationBar
+                pagination={pagination}
+                setPagination={setPagination}
+            />
         </ListLayout>
     );
 };
