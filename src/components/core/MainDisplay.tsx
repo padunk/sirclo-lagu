6d1c9144d57eb09e0ad38a771c40c5f38a0fd2c6
padunk <abraham.agel@gmail.com>
2021-08-02 08:47:27 +0700

    feat: search feature

--- src/components/core/MainDisplay.tsx
+++ src/components/core/MainDisplay.tsx
@@ -1,23 +1,39 @@
-import { Flex } from "@chakra-ui/react";
-import React, { ChangeEvent, useState } from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { ShowBy } from "../../types";
-import ArtistList from "./ArtistList";
 import ListMenu from "./ListMenu";
+import ArtistList from "./ArtistList";
 import SongList from "./SongList";
+import SearchList from "./SearchList";
+import SearchTemplate from "../utils/SearchTemplate";
 
 interface Props {}
 
 const MainDisplay = (props: Props) => {
-    const [showtBy, setShowtBy] = useState<ShowBy>(ShowBy.Artist);
+    const [showBy, setShowBy] = useState<ShowBy | null>(ShowBy.Artist);
+    const [searchTerms, setSearchTerms] = useState("");
 
     const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        setShowtBy(e.target.value as ShowBy);
+        setShowBy(e.target.value as ShowBy);
     };
 
     return (
-        <Flex direction="column">
-            <ListMenu onChange={handleChange} />
-            {showtBy === "topTrack" ? <SongList /> : <ArtistList />}
+        <Flex direction="column" flexGrow={2}>
+            <ListMenu
+                onChange={handleChange}
+                searchTerms={searchTerms}
+                setSearchTerms={setSearchTerms}
+                setShowBy={setShowBy}
+            />
+            {showBy === ShowBy.Artist ? (
+                <ArtistList />
+            ) : showBy === ShowBy.Track ? (
+                <SongList />
+            ) : searchTerms === "" ? (
+                <SearchTemplate />
+            ) : (
+                <SearchList searchTerms={searchTerms} />
+            )}
         </Flex>
     );
 };
