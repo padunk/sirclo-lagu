654700d9d0e95eb8822f66a25baf27c884024d66
padunk <abraham.agel@gmail.com>
2021-08-02 16:06:28 +0700

    fix only one tag or undefined

--- src/components/utils/Tags.tsx
+++ src/components/utils/Tags.tsx
@@ -3,10 +3,25 @@ import React from "react";
 import { Tag } from "../../types";
 
 interface Props {
-    tag: Tag[] | undefined;
+    tag: Tag[] | undefined | Tag;
 }
 
 const Tags = ({ tag }: Props) => {
+    if (tag === undefined) {
+        return "";
+    }
+    // if only one tag
+    if (!Array.isArray(tag) && typeof tag === "object") {
+        return (
+            <Box d="flex" flexWrap="wrap" w="full" mt="2">
+                <Box as="a" href={tag.url} key={tag.name} mr="1">
+                    <Badge px="2" colorScheme="pink" borderRadius="full">
+                        {tag.name}
+                    </Badge>
+                </Box>
+            </Box>
+        );
+    }
     return (
         <Box d="flex" flexWrap="wrap" w="full" mt="2">
             {tag?.map((t) => {
