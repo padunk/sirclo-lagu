a8acd35132aaa6bed41b479618ae3fc8ea993648
padunk <abraham.agel@gmail.com>
2021-08-03 16:08:26 +0700

    fix precison result due to toFixed rounded float

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -5,7 +5,7 @@ export function timeFormatter(time: number): string {
     return `${mins}:${String(secs).padStart(2, "0")}`;
 }
 
-export function numberFormatter(n: number | string | undefined): {
+export function numberFormatter(n?: number | string): {
     formattedNumber: string;
     satuan: string;
 } {
@@ -16,14 +16,27 @@ export function numberFormatter(n: number | string | undefined): {
     // change n type to number
     let num = Number(n);
     if (num > 999_999) {
-        result.formattedNumber = (num / 1_000_000).toFixed(2);
+        result.formattedNumber = toPreciseString(num, 1000000);
         result.satuan = "M";
     } else if (num > 999) {
-        result.formattedNumber = (num / 1_000).toFixed(2);
+        result.formattedNumber = toPreciseString(num, 1000);
         result.satuan = "K";
     } else {
         result.formattedNumber = String(num);
     }
 
     return result;
+}
+
+/**
+ * We use this function because toFixed() will round float number
+ * @param n number, number to divide
+ * @param satuan number, unit to divide
+ * @returns string in two decimal digit precision
+ */
+function toPreciseString(n: number, unit: number): string {
+    let num = n / unit;
+    let numStr = Number.parseFloat(String(num)).toPrecision(String(num).length);
+    numStr = numStr.slice(0, numStr.indexOf(".") + 3);
+    return numStr;
 }
