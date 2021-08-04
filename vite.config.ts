20ff8ffcd1b687f74a8daa34af179687a6bbf0f2
padunk <abraham.agel@gmail.com>
2021-08-04 09:05:35 +0700

    update define

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh()],
+    plugins: [...(process.env.NODE_ENV !== "test" ? [reactRefresh()] : [])],
+    define: {
+        "process.env.VITE_LAST_FM_API_KEY": `"${process.env.VITE_LAST_FM_API_KEY}"`,
+    },
 });
