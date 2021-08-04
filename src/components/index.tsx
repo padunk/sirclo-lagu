e7a1e3f8a783ece483a6ca02621cdf8b38386236
padunk <abraham.agel@gmail.com>
2021-08-04 09:06:50 +0700

    chore: index component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,8 @@
 export { default as MainHeader } from "./core/MainHeader";
+export { default as Footer } from "./core/Footer";
+export { default as MainDisplay } from "./core/MainDisplay";
+export { default as ListMenu } from "./core/ListMenu";
+export { default as ArtistList } from "./core/ArtistList";
 
 export { default as Search } from "./utils/Search";
+export { default as SearchModal } from "./utils/SearchModal";
