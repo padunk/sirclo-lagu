e1192a26285a9ece28ea9f33ea3f4145c605fac0
padunk <abraham.agel@gmail.com>
2021-08-09 09:27:45 +0700

    v.2.0: refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
+import React from "react";
 import { Flex } from "@chakra-ui/react";
-import React, { useState } from "react";
 import { MainHeader } from "./components";
 import Footer from "./components/core/Footer";
 import MainDisplay from "./components/core/MainDisplay";
-import { listLoading, LoadingContext } from "./context/LoadingContext";
+// import MainDisplay from "./components/copy/MainDisplay.copy";
+// import { MusicProvider, QueryPropsProvider } from "./hooks/apiHooks";
 
 function App() {
     return (
-        <LoadingContext.Provider value={listLoading()}>
-            <Flex direction="column" bg="gray.50" minH="100vh">
-                <MainHeader />
-                <MainDisplay />
-                <Footer />
-            </Flex>
-        </LoadingContext.Provider>
+        // <QueryPropsProvider>
+        //     <MusicProvider>
+        <Flex direction="column" bg="gray.50" minH="100vh">
+            <MainHeader />
+            <MainDisplay />
+            <Footer />
+        </Flex>
+        //     </MusicProvider>
+        // </QueryPropsProvider>
     );
 }
 
